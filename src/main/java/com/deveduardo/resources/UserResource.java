cb2db1135424e13f2be692cc73f2b9e0d33374bd
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-10 23:07:10 -0300

    User entity and resource

---
+++ src/main/java/com/deveduardo/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.deveduardo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.deveduardo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		System.out.println("Endpoint /users foi chamado");
+		User u = new User(1L, "Maria", "Maria@gmail.com", "9999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
