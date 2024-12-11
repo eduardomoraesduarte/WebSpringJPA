bd8fdc3f151ec6640191389d54aa00cce50d642e
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-10 22:15:14 -0300

    Create Project

---
+++ src/main/java/com/deveduardo/WebSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.deveduardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveduardo/WebSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
