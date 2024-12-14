0ec0f8b1967dca2d45896be29ad7cf8b1d939474
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-13 23:31:06 -0300

    Exception handling - delete

--- src/main/java/com/deveduardo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/deveduardo/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.deveduardo.services.exceptions.DatabaseException;
 import com.deveduardo.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,13 +17,23 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e,
+			HttpServletRequest request) {
 		String error = "Resource Not Found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
